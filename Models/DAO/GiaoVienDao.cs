1db7b21d8a4113eb59b315a12336a32e448dbd41
tamle-dev <57066462+tamle-dev@users.noreply.github.com>
2020-10-16 02:16:14 +0700

    delete_giao_vien

---
+++ Models/DAO/GiaoVienDao.cs
@@ -0,0 +1,87 @@
+﻿using Models.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using PagedList;
+using System;
+
+namespace Models
+{
+    public class GiaoVienDao
+    {
+        // Context : Lớp ngữ cảnh kế thừa từ DBContext
+        private eCenterDbContext  context = null;
+
+        public GiaoVienDao()
+        {
+            // Khởi tạo ngữ cảnh 
+            context = new eCenterDbContext();
+        }
+
+        // Method : Insert GiaoVien entity
+        public int Insert(GiaoVien entity)
+        {
+            var ngayDangKy = DateTime.Now;
+            entity.NgayDangKy = ngayDangKy;
+
+            context.GiaoViens.Add(entity);
+            context.SaveChanges();
+            return entity.MaGiaoVien;
+        }
+
+        public GiaoVien GetById(string tenGiaoVien)
+        {
+            return context.GiaoViens.SingleOrDefault(x=>x.TenGiaoVien == tenGiaoVien);
+        }
+
+        public IEnumerable<GiaoVien> ListAllPaging(int page, int pageSize)
+        {
+
+            return context.GiaoViens.OrderBy(x=>x.MaGiaoVien).ToPagedList(page,pageSize);
+        }
+
+        public bool Update(GiaoVien entity)
+        {
+            try
+            {
+                var giaoVien = context.GiaoViens.Find(entity.MaGiaoVien);
+                giaoVien.TenGiaoVien = entity.TenGiaoVien;
+                giaoVien.GioiTinh = entity.GioiTinh;
+                giaoVien.NgaySinh = entity.NgaySinh;
+                giaoVien.Email = entity.Email;
+                giaoVien.SDT = entity.SDT;
+                giaoVien.GhiChu = entity.GhiChu;
+                giaoVien.DiaChi = entity.DiaChi;
+                giaoVien.QuocTich = entity.QuocTich;
+                giaoVien.TrangThai = entity.TrangThai;
+                context.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public GiaoVien ViewDetail(int id)
+        {
+            return context.GiaoViens.Find(id); 
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var giaoVien = context.GiaoViens.Find(id);
+                context.GiaoViens.Remove(giaoVien);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+          
+        }
+    }
+}
---
+++ QuanLyTrungTam/Controllers/GiaoVienController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Web.Mvc;
+using Models;
+using Models.Framework;
+using PagedList;
+
+// DAO = Data Access Object
+
+namespace QuanLyTrungTam.Controllers
+{
+    public class GiaoVienController : Controller
+    {
+        // GET: GiaoVien
+        public ActionResult Index(int page = 1,int pageSize = 1)
+        {
+            var daoGiaoVien = new GiaoVienDao();
+            var modelGiaoVien = daoGiaoVien.ListAllPaging(page, pageSize);
+
+            return View(modelGiaoVien);
+        }
+
+        // GET: GiaoVien/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GiaoVien/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            //var dao = new GiaoVienDao();
+
+            return View();
+        }
+
+        // POST: GiaoVien/Create
+        [HttpPost]
+        public ActionResult Create(GiaoVien giaoVien)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    var daoGiaoVien = new GiaoVienDao();    
+                    int layMaGiaoVien = daoGiaoVien.Insert(giaoVien);
+                   
+                    if(layMaGiaoVien > 0)
+                    {
+                        return RedirectToAction("Index", "GiaoVien");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Thêm thất bại");
+                    }
+                }
+                return View(giaoVien);
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        // GET: GiaoVien/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var _giaoVien = new GiaoVienDao().ViewDetail(id);
+
+            return View(_giaoVien);
+        }
+
+        // POST: GiaoVien/Edit/5
+        [HttpPost]
+        public ActionResult Edit(GiaoVien giaoVien)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    var daoGiaoVien = new GiaoVienDao();
+
+                    var res = daoGiaoVien.Update(giaoVien);
+                    if (res)
+                    {
+                        return RedirectToAction("Index", "GiaoVien");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Thêm thất bại");
+                    }
+                }
+                return View(giaoVien);
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        // GET: GiaoVien/Delete/5
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        // POST: GiaoVien/Delete/5
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                new GiaoVienDao().Delete(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
